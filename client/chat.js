4ec3bf62080bd8ab281b83c36b1c44f854ee2920
RomaP28 <posohovrom@icloud.com>
2022-02-15 16:59:52 +0200

    Implementing Chat

--- client/chat.js
+++ client/chat.js
@@ -1,22 +1,24 @@
-const getUserName = () => {
-  prompt("Please enter your name");
-}
-
-// const name = getUserName();
-
 let socket = new WebSocket("ws://localhost:8080");
 
-document.forms.publish.onsubmit = () => { //клик по кнопке send
-  console.log('Connected');
-  console.log('Отправляем данные на сервер');
-  let outgoingMessage = document.forms.publish.message.value;
-  socket.send(outgoingMessage);
+if (userName === '') {
+  let userName = prompt("Please enter your name");
 }
 
-socket.onmessage = function (event) {
+document.forms.publish.onsubmit = () => { //клик по кнопке send
+  let outgoingMessage = document.forms.publish.message.value + '|' + userName;
+  socket.send(outgoingMessage); //отправляем сообщение
+}
+
+socket.onmessage = function (event) {   //принимаем сообщение
   console.log(`Данные получены с сервера: ${event.data}`);
   let messageElem = document.createElement('div');
-  messageElem.setAttribute('class', 'message')
-  messageElem.textContent = event.data;
+  let message = document.createElement('div');
+  let p = document.createElement('p');
+  p.setAttribute('class', 'userName');
+  p.innerHTML = event.data.substring(event.data.lastIndexOf('|') + 1);
+  message.setAttribute('class', 'message')
+  message.textContent = event.data.split('|')[0];
+  messageElem.appendChild(p);
+  messageElem.appendChild(message);
   document.getElementById('messages').prepend(messageElem);
 };
